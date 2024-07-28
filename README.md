80c1c1f4001a34910cdc2fbb8a763e3442f629ca
huangxulei <pjhxl@qq.com>
2024-07-28 17:26:28 +0800

    728

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # TXT 文件阅读器
 
 1. 显示某个 TEXT 文件
+   开发目录
+   SearchItem
+   ==> List<ChapterItem> chapters
+   ==> id 时间戳
+   ChapterItem
+   ==> name 标题 url "0.txt" 文件名
+
+2. 从 assets 导入数据库文件 novel.db
+
+    splash
+
+3. 导入 txt 文件 分割成小文件
+
+    sqlite 数据库 + 文件保存
+
+    数据库 txt
+
+    novel 表
+
+    novelId 自增 title 名字 addTime 添加时间 时间戳
+
+    chapter 章表
+    chapterId 自增 novelId /上级 id sid 排序号 title 章名
---
+++ lib/sql_helper.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+class SQLHelper {
+  static Future<sql.Database> db() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, 'novel.db');
+    return await sql.openDatabase(path);
+  }
+
+  static Future<List<Map<String, dynamic>>> getNovels() async {
+    final db = await SQLHelper.db();
+    return db.query('novel', orderBy: 'novelId');
+  }
+
+  static Future<void> initialDatabase() async {
+    sqfliteFfiInit();
+    var databaseFactory = databaseFactoryFfi;
+    var databasesPath = await databaseFactory.getDatabasesPath();
+    var path = join(databasesPath, "novel.db");
+    var exists = await databaseFactory.databaseExists(path);
+    if (!exists) {
+      try {
+        await Directory(dirname(path)).create(recursive: true);
+      } catch (_) {}
+      ByteData data = await rootBundle.load('assets/novel.db');
+      // ByteData data = await rootBundle.load(join('assets','express.db'));
+      List<int> bytes =
+          data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+      // Write and flush the bytes written
+      await File(path).writeAsBytes(bytes, flush: true);
+    } else {
+      print("Opening existing database");
+    }
+  }
+}
